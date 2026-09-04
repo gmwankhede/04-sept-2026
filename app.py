2bf7768b0ac7241c9d6d69b221f84c0a0e4cff38
gmwankhede <121442287+gmwankhede@users.noreply.github.com>
2026-09-04 18:41:10 +0530

    Update print statements in home function

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("Python program syntax update successfully.")
-    print("Python program second line update successfully.")
-    return "Hello from GitHub Actions and Azure Web App!"
+    print("Python program Ganesh and Sonu successfully.")
+    print("Python program second line update by  Ganesh and Sonu  successfully.")
+    return "Hello from GitHub Actions and Azure Web App! by  Ganesh and Sonu "
 
 if __name__ == "__main__":
     app.run()
