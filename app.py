fd7163faf452c8e013fbbd1335ada9c05cc072f9
gmwankhede <121442287+gmwankhede@users.noreply.github.com>
2026-09-04 18:40:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    print("Python program syntax update successfully.")
+    print("Python program second line update successfully.")
     return "Hello from GitHub Actions and Azure Web App!"
 
 if __name__ == "__main__":
